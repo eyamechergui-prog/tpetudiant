2b00cd0ed9ce1da3ae3033ae578e0f8af96c3051
eyamechergui-prog <eya.mechergui@isgb.ucar.tn>
2025-12-18 20:37:07 +0100

    Ajout du projet gestion_des_horaires_de_bus avec code et configuration .gitignore

---
+++ Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Btn_showMsg = New Button()
+        SuspendLayout()
+        ' 
+        ' Btn_showMsg
+        ' 
+        Btn_showMsg.BackColor = Color.Lime
+        Btn_showMsg.Location = New Point(309, 104)
+        Btn_showMsg.Name = "Btn_showMsg"
+        Btn_showMsg.Size = New Size(137, 65)
+        Btn_showMsg.TabIndex = 0
+        Btn_showMsg.Text = "Click Me"
+        Btn_showMsg.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Btn_showMsg)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Btn_showMsg As Button
+
+End Class
